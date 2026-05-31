0c467469ec2c309b8c2f421e197ade9f9be95093
masha.kasatkina <mariia.kasatkina@dif.tech>
2026-05-31 14:44:04 +0400

    feat_68: two tests work fine

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,7 +72,6 @@ dependencies {
     implementation(libs.coil.svg)
     implementation(libs.compose.ui)
     implementation(libs.androidx.viewmodel)
-    implementation(libs.androidx.ui.test.junit4)
 
     testImplementation(libs.junit4)
     testImplementation(libs.coroutines.test)
