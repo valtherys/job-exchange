42db5c137e804f7718f4fd69bcd4094bf448bb17
masha.kasatkina <mariia.kasatkina@dif.tech>
2026-06-01 18:15:08 +0400

    feat_75: scraps

--- app/src/main/java/ru/practicum/android/diploma/ui/filtration/screen/FiltrationScreen.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/filtration/screen/FiltrationScreen.kt
@@ -1,10 +1,239 @@
 package ru.practicum.android.diploma.ui.filtration.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsFocusedAsState
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.ui.common.IconImage
+import ru.practicum.android.diploma.ui.common.TopBar
+import ru.practicum.android.diploma.ui.search.screen.SearchScreenTestTags
+import ru.practicum.android.diploma.ui.theme.Blue
+import ru.practicum.android.diploma.ui.theme.Dimens
 
 @Composable
-fun FiltrationScreen(modifier: Modifier = Modifier) {
-    Box(modifier = modifier)
+fun FiltrationScreen(
+    modifier: Modifier = Modifier,
+    country: String = "Россия",
+    onCheckedChange: (Boolean) -> Unit = {}
+) {
+    val keyboardController = LocalSoftwareKeyboardController.current
+    val interactionSource = remember { MutableInteractionSource() }
+
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            TopBar(
+                text = stringResource(R.string.filter_settings_title),
+                navIconVisible = true,
+                endFirstIconVisible = false,
+                endSecondIconVisible = false
+            )
+        },
+        contentWindowInsets = WindowInsets(bottom = 0),
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier.padding(paddingValues)
+        ) {
+            CellComponent(
+                upperText = stringResource(R.string.filter_settings_country_title),
+                lowerText = country,
+                trailingIconId = R.drawable.ic_arrow_right
+            )
+            CellComponent(
+                upperText = stringResource(R.string.specialization_title),
+                lowerText = country,
+                trailingIconId = R.drawable.ic_arrow_right
+            )
+           SalaryTextEdit(
+               searchQuery = "",
+               interactionSource = interactionSource,
+               onSearchTextChange = {},
+               onClear = {},
+               onKeyboardDone = { keyboardController?.hide() }
+           )
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp)
+            ) {
+                Text(
+                    text = stringResource(R.string.filter_settings_country_title),
+                    style = MaterialTheme.typography.headlineMedium
+                )
+                Checkbox(
+                    enabled = true,
+                    checked = true,
+                    onCheckedChange = onCheckedChange
+                )
+            }
+        }
+    }
 }
+
+@Composable
+fun SalaryTextEdit(
+    searchQuery: String,
+    interactionSource: MutableInteractionSource,
+    onSearchTextChange: (String) -> Unit,
+    onClear: () -> Unit,
+    onKeyboardDone: () -> Unit,
+) {
+    val fieldShape = RoundedCornerShape(8.dp)
+    val isFocused by interactionSource.collectIsFocusedAsState()
+    val labelColor = if ( isFocused) {
+        MaterialTheme.colorScheme.primary
+    } else {
+        if( searchQuery.isEmpty()) {
+            MaterialTheme.colorScheme.inverseOnSurface
+        } else {
+            MaterialTheme.colorScheme.secondaryFixed
+        }
+    }
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(
+                start = Dimens.ScreenHorizontalPadding,
+                top = 8.dp,
+                end = Dimens.ScreenHorizontalPadding,
+            )
+            .height(56.dp)
+            .clip(fieldShape)
+            .background(MaterialTheme.colorScheme.surfaceContainer),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        BasicTextField(
+            value = searchQuery,
+            onValueChange = onSearchTextChange,
+            modifier = Modifier
+                .weight(1f)
+                .testTag(SearchScreenTestTags.TextField)
+                .fillMaxHeight()
+                .padding(start = 20.dp),
+            singleLine = true,
+            textStyle = MaterialTheme.typography.bodyMedium
+                .copy(color = MaterialTheme.colorScheme.secondaryFixed),
+            cursorBrush = SolidColor(Blue),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Done
+            ),
+            keyboardActions = KeyboardActions(onDone = { onKeyboardDone() }),
+            interactionSource = interactionSource,
+            decorationBox = { innerTextField ->
+                Box(
+                    Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.CenterStart
+                ) {
+                    Column(
+                        modifier = Modifier.padding(start = 16.dp, top = 8.dp)
+                    ) {
+                        Text(
+                            text = stringResource(R.string.filter_settings_salary_title),
+                            style = MaterialTheme.typography.bodySmall,
+                            color = labelColor
+                         )
+                        if (searchQuery.isEmpty()) {
+                            Text(
+                                text = stringResource(R.string.input_amount_hint),
+                                style = MaterialTheme.typography.bodyMedium.copy(
+                                    color = MaterialTheme.colorScheme.inverseOnSurface
+                                ),
+                                maxLines = 1,
+                             )
+                        }
+                        innerTextField()
+                    }
+                }
+            }
+        )
+        IconImage(
+            modifier = Modifier
+                .testTag(SearchScreenTestTags.ClearButton)
+                .padding(end = 4.dp)
+                .clickable(enabled = true, onClick = onClear),
+            resId = R.drawable.ic_cross,
+            color = MaterialTheme.colorScheme.secondaryFixed
+        )
+    }
+}
+
+@Composable
+fun CellComponent(
+    upperText: String,
+    lowerText: String,
+    trailingIconId: Int,
+    modifier: Modifier = Modifier,
+    onIconClick: () -> Unit = {},
+ ) {
+    Row(
+        modifier = modifier
+             .fillMaxWidth()
+             .padding(horizontal = 16.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .align(Alignment.CenterVertically)
+                .weight(1f)
+                .padding(start = 16.dp)
+        ) {
+            Text(
+                text = upperText,
+                style = MaterialTheme.typography.headlineMedium
+             )
+            Text(
+                text = lowerText,
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+        IconImage(
+            modifier = Modifier
+                .align(Alignment.CenterVertically)
+                .testTag(SearchScreenTestTags.ClearButton)
+                .padding(end = 4.dp)
+                .clickable(enabled = true, onClick = onIconClick),
+            resId = if (lowerText.isEmpty()) trailingIconId else R.drawable.ic_cross,
+            color = MaterialTheme.colorScheme.onBackground
+        )
+    }
+}
+
+@Composable
+@Preview
+fun PreviewFiltrationScreen() {
+    FiltrationScreen()
+}
+
