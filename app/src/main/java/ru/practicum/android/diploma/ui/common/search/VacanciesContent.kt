a03c6336331a013ff33529d32bb4f70ca3acc0c9
valtherys <melikhovainna@gmail.com>
2026-06-05 15:31:44 +0300

    fix: ворнинги detekt [#101]

--- app/src/main/java/ru/practicum/android/diploma/ui/common/search/VacanciesContent.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/common/search/VacanciesContent.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
