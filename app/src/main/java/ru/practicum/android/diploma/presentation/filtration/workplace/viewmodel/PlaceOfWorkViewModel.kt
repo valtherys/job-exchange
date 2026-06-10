74d27c70e7b1f470c06e1b74711d41046716504e
valtherys <melikhovainna@gmail.com>
2026-06-10 12:28:23 +0300

    fix: при выборе региона подтягивается страна

--- app/src/main/java/ru/practicum/android/diploma/presentation/filtration/workplace/viewmodel/PlaceOfWorkViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/filtration/workplace/viewmodel/PlaceOfWorkViewModel.kt
@@ -2,14 +2,18 @@ package ru.practicum.android.diploma.presentation.filtration.workplace.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import ru.practicum.android.diploma.domain.api.AreaInteractor
 import ru.practicum.android.diploma.presentation.filtration.workplace.state.AreaUi
 import ru.practicum.android.diploma.presentation.filtration.workplace.state.PlaceOfWorkUIState
 
-class PlaceOfWorkViewModel : ViewModel() {
+class PlaceOfWorkViewModel(private val areaInteractor: AreaInteractor) : ViewModel() {
     private val countryFlow = MutableStateFlow<AreaUi?>(null)
     private val regionFlow = MutableStateFlow<AreaUi?>(null)
 
@@ -23,7 +27,16 @@ class PlaceOfWorkViewModel : ViewModel() {
     }
 
     fun onRegionSelected(region: AreaUi) {
-        regionFlow.value = region
+        viewModelScope.launch {
+            if (countryFlow.value == null) {
+                val response = withContext(Dispatchers.IO) {
+                    areaInteractor.getParentByRegionId(region.id)
+                }
+                countryFlow.value = response?.let { AreaUi(it.id, it.name) }
+            }
+
+            regionFlow.value = region
+        }
     }
 
     fun onDeleteCounty() {
